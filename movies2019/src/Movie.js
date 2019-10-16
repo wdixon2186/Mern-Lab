9fbee928d9c99f468b6d3db6716fcd5fe840105b
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-16 13:33:40 -0700

    created base functionality

--- movies2019/src/Movie.js
+++ movies2019/src/Movie.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
 import './Movie.css';
+import axios from "axios";
 import listOfMovies from "./data/movie-data.json";
 import { Route, Link, Switch } from "react-router-dom";
 
 class Movie extends Component {
+  constructor() {
+    super()
+
+    this.state = {movie: {}}
+  }
+  componentDidMount() {
+		axios
+			.get(`http://localhost:8080/${this.props.match.params.title}`)
+			.then(res => {
+				console.log(res);
+				this.setState({ movie: res.data });
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
   render() {
-    let list = listOfMovies;
-    let movieToDisplay = list.find(element => {
-      return element.title === this.props.match.params.title;
-    });
+    // let list = listOfMovies;
+    // let movieToDisplay = list.find(element => {
+    //   return element.title === this.props.match.params.title;
+    // });
 
     return (
-      <div>
-        <h1>{movieToDisplay.title}</h1>
-        <img class='imgMovie' src={movieToDisplay.image}></img>
-        <p>Director: {movieToDisplay.director}</p>
-        <p>Writers: {movieToDisplay.writer}</p>
-        <a class='movie' href={movieToDisplay.homePage}>Read More</a>
+      // <div>
+      //   <h1>{movieToDisplay.title}</h1>
+      //   <img class='imgMovie' src={movieToDisplay.image}></img>
+      //   <p>Director: {movieToDisplay.director}</p>
+      //   <p>Writers: {movieToDisplay.writer}</p>
+      //   <a class='movie' href={movieToDisplay.homePage}>Read More</a>
         
-      </div>
+      // </div>
+      <div>
+      <h1>{this.state.movie.title}</h1>
+      <img class='imgMovie' src={this.state.movie.image}></img>
+      <p>Director: {this.state.movie.director}</p>
+      <p>Writers: {this.state.movie.writer}</p>
+      <a class='movie' href={this.state.movie.homePage}>Read More</a>
+      
+    </div>
     );
   }
 }
--- movies2019/src/MovieList.js
+++ movies2019/src/MovieList.js
@@ -1,23 +1,55 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import listOfMovies from "./data/movie-data.json";
 import { Route, Link, Switch } from "react-router-dom";
-import './MovieList.css';
+import "./MovieList.css";
 
 class MovieList extends Component {
+	constructor() {
+		super();
+
+		this.state = { data: [] };
+	}
+	componentDidMount() {
+		axios
+			.get("http://localhost:8080/")
+			.then(res => {
+				console.log(res);
+				this.setState({ data: res.data });
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
 	render() {
-		let list = listOfMovies.map(item => {
+		// let list = listOfMovies.map(item => {
+		// 	return (
+		// 		<div key={item.title}>
+		// 			<p>
+		// 				<Link to={"/" + item.title}>
+		// 					<img class="img" src={item.image}></img>
+		// 				</Link>
+		// 			</p>
+		// 		</div>
+		// 	);
+		// });
+		let list = this.state.data.map(item => {
 			return (
 				<div key={item.title}>
 					<p>
 						<Link to={"/" + item.title}>
-							<img class='img' src={item.image}></img>
+							<img class="img" src={item.image}></img>
 						</Link>
 					</p>
 				</div>
 			);
 		});
-		return <div>{list}</div>;
+		return (
+			<div>
+				{list}
+				<a href=''>CREATE</a>
+			</div>
+		);
 	}
 }
 
