d8c76fcdd21a711473a09643492df7773455de73
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-16 09:14:42 -0700

    initial setup

---
+++ movies2019/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link, Switch, Redirect } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to='/'>
+          <h1>Home</h1>
+        </Link>
+      </nav>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
